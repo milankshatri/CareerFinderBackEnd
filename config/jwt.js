4fdce6462a04a13c7b5b8afe1fde95af810b5937
milankshatri <milankshatri17@gmail.com>
2023-09-02 15:15:32 +0545

    testing

--- config/jwt.js
+++ config/jwt.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const createToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: '24h'});
+    return jwt.sign({ id }, process.env.JWT_SECRET);
 }
 
 module.exports = createToken;
